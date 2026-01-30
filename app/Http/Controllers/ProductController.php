e4d28b3987a5a91098a593c85d85f06ca9ebaeed
yusho-aoyama <yusho-aoyama>
2026-01-30 08:22:26 +0800

    Day7: Update & Delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,13 +10,23 @@ class ProductController extends Controller
     // Add an index method
     public function index(Request $request)
     {
-        $category = $request->query('category');
+//        $category = $request->query('category');
+//
+//        if ($category) {
+//            $products = Product::where('category', $category)->get();
+//        } else {
+//            $products = Product::all();
+//        }
+//        return response()->json($products);
 
-        if ($category) {
-            $products = Product::where('category', $category)->get();
+        $search = $request->query('search');
+//        dd($search);
+        if ($search) {
+            $products = Product::where('name', 'like', '%' . $search . '%')->get();
         } else {
             $products = Product::all();
         }
+
         return response()->json($products);
     }
 
@@ -36,5 +46,44 @@ class ProductController extends Controller
                 'message' => 'Product created successfully',
                 'product' => $product
             ]);
+    }
+
+    // Add an Update method
+    public function update(Request $request, $id)
+    {
+        // 1, Find product
+        $product = Product::findOrFail($id);
+
+        // 2, Validation
+        $validated = $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'category' => 'required|string'
+        ]);
+
+        // 3, Update
+        $product->update($validated);
+
+        //4 Response
+        return response()->json([
+            'message' => 'Product updated successfully',
+            'product' => $product
+        ]);
+
+    }
+
+    // Add a delete method
+    public function destroy($id)
+    {
+        // 1, Find product
+        $product = Product::findOrFail($id);
+
+        // 2, Delete product
+        $product->delete();
+
+        // 3, Response
+        return response()->json([
+            'message' => 'Product deleted successfully'
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,6 @@ Route::get('/test', [TestController::class, 'index']);
 // Routes for ProductController
 Route::post('product', [ProductController::class, 'store']);
 Route::get('/products',[ProductController::class, 'index']);
-
+// CRUD
+Route::put('/products/{id}',[ProductController::class, 'update']);
+Route::delete('/products/{id}',[ProductController::class, 'destroy']);
